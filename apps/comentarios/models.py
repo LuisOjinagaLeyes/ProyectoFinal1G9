0719e4223e799454584ee0aeb22e62c998d1a3a4
LuisOjinagaLeyes <luisojinaga19@gmail.com>
2023-12-27 20:44:52 -0300

    modificacion de index_noticias.html,detalle_noticia...etc

--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from django.contrib.auth.models import User
 from apps.noticias.models import Noticia
 
@@ -14,6 +15,16 @@ class Comentario(models.Model):
 	texto = models.TextField()
 	usuario = models.ForeignKey(User, on_delete = models.CASCADE)
 	noticia = models.ForeignKey(Noticia, on_delete = models.CASCADE)
+	fecha = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.texto
+
+class Form_Modificacion(forms.ModelForm):
+
+	class Meta:
+		model = Comentario
+		fields = ('texto',)
+		widgets = {
+			'texto': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Escribe tu comentario...', 'required': 'true'}),
+		}
